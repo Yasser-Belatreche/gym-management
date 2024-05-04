de1c80f13a24204059cff19394ee034188c58473
Yasser-Belatreche <yasser.belatreche0@gmail.com>
2024-05-04 01:49:56 +0100

    fix build

--- src/components/managers.go
+++ src/components/managers.go
@@ -1,9 +1,12 @@
 package components
 
-import "gym-management/src/components/gyms"
+import (
+	"gym-management/src/components/gyms"
+	"gym-management/src/lib"
+)
 
 func InitManagers() {}
 
 func Gyms() gyms.Manager {
-	return gyms.NewGymsManager()
+	return gyms.NewGymsManager(lib.MessagesBroker())
 }
