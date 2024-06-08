836eccc0ba7f09bfd92c41aa8746dbd8ea580c25
Yasser-Belatreche <yasser.belatreche0@gmail.com>
2024-06-08 17:26:37 +0100

    First Version

--- microservices/auth-service/scripts/apply-migrations.sh
+++ microservices/auth-service/scripts/apply-migrations.sh
@@ -2,4 +2,11 @@
 
 set -e
 
+# Check if atlas command exists
+if ! command -v atlas &> /dev/null
+then
+    echo "atlas could not be found, downloading..."
+    curl -sSf https://atlasgo.sh | sh
+fi
+
 atlas migrate apply --env gorm
